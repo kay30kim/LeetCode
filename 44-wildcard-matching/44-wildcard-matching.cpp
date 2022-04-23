59b93ad56e9df944055aaeb24555433294c1a50f
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2022-04-23 09:57:12 +0900

    Time: 147 ms (17.90%), Space: 11.3 MB (54.55%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
+    // s = abc p = b*c
+    // s = abc p = *
     bool isMatch(string s, string p) {
         int n = s.size(), m = p.size();
         vector<vector<bool>> dp(n+1,vector<bool>(m+1,0));
         
-        dp[0][0]=1;
-        for(int j=0; j<m; j++){
-            if (p[j] == '*' && dp[0][j] == true){
-                dp[0][j+1] = true;
+        // dp[n][m] means string s and string p are same until index 0 to index n and index 0 to index m
+        dp[0][0] = true;
+        for(int i=0; i<m; i++){
+            if(p[i]=='*' && dp[0][i] == true){
+                dp[0][i+1] = true;
             }
         }
+        
         for(int i=0; i<n; i++){
             for(int j=0; j<m;j++){
-                if(p[j] =='?' || p[j] == s[i]){
+                if(p[j]=='?' || p[j] ==s[i]){
                     dp[i+1][j+1] = dp[i][j] & true;
-                }else if(p[j] == '*'){
+                }else if(p[j]=='*'){
                     dp[i+1][j+1] = dp[i][j] & true;
-                    if (dp[i+1][j+1] == false) dp[i+1][j+1] = dp[i][j+1] & true;
-                    if (dp[i+1][j] == true) dp[i+1][j+1] = true;
+                    if(dp[i+1][j+1] == false) dp[i+1][j+1] = dp[i][j+1] & true;
+                    if(dp[i+1][j] == true) dp[i+1][j+1] = true;
                 }
             }
         }
