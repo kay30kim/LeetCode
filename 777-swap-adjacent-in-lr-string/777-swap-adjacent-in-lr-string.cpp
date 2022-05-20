93759cee1ff64aa0469a2275e5e963a0df978f8b
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2022-05-20 23:39:38 +0900

    Time: 13 ms (19.29%), Space: 7.5 MB (40.24%) - LeetHub

--- 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.cpp
+++ 777-swap-adjacent-in-lr-string/777-swap-adjacent-in-lr-string.cpp
@@ -3,42 +3,22 @@ private:
     unsigned int arr[3]={1,10001,20001};
 public:
     bool canTransform(string start, string end) {
-        unsigned int sNum=0, eNum=0;
-        for(int i=0; start[i]; i++){
-            if(start[i]=='L'){
-                sNum+=arr[0];
-            }else if(start[i] =='R'){
-                sNum+=arr[1];
-            }else{
-                sNum+=arr[2];
-            }
-        }
-        for(int i=0; end[i]; i++){
-            if(end[i]=='L'){
-                eNum+=arr[0];
-            }else if(end[i] =='R'){
-                eNum+=arr[1];
-            }else{
-                eNum+=arr[2];
-            }
-        }
-        if(sNum==eNum) {
 // skip 'X', make sure start[i] and end[j] is the same
 // 'R' position in start is smaller than end
 // 'L' position in start is larger than end
-            int i=0, j=0;
-            while(i<start.size() && j<end.size()) {
+        int i=0, j=0;
+        while(i<start.size() && j<end.size()) {
 
-                while(start[i]=='X') i++;
-                while(end[j]=='X') j++;
+            while(start[i]=='X') i++;
+            while(end[j]=='X') j++;
 
-                if(start[i]!=end[j]) return false;
-                if(start[i]=='R' && i>j) return false;
-                if(start[i]=='L' && i<j) return false;
-                i++; j++;
-            }
-            return true;
+            if(start[i]!=end[j]) return false;
+            if(start[i]=='R' && i>j) return false;
+            if(start[i]=='L' && i<j) return false;
+            i++; j++;
         }
-        else return false;
+        while(i<start.size() && start[i]=='X') i++;
+        while(j<end.size() && end[j]=='X') j++;
+        return i==j;
     }
 };
