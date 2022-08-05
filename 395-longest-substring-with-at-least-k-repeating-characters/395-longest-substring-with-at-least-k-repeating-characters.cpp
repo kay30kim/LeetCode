0457d9799bd229cfd10967b300a7e2306870b3a9
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2022-08-05 10:20:56 +0900

    Time: 0 ms (100.00%), Space: 6.4 MB (87.69%) - LeetHub

--- 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/395-longest-substring-with-at-least-k-repeating-characters.cpp
@@ -22,14 +22,11 @@ public:
     int longestSubstring(string s, int k) {
         // clear();
         int maxAlphabet = fun.numUniqChar(s), ans=0;
-        //printf("%d\n",maxAlphabet);
         for(int nAlphabet=1; nAlphabet<=maxAlphabet; nAlphabet++){
             clear();
             int len=0, nowNAlphabet = 0, p1=0, p2=0, sAlphabet = 0;
             while(p2 < s.size()){
                 //if(nChar[s[p2]-'a'])
-                // printf("now = %d  이번 턴 최대 알파벳 수 = %d, p1 = %d p2 = %d len = %d\n", nowNAlphabet, nAlphabet,p1,p2,len);
-                // printf("nChar = %d\n",nChar[s[p2]-'a']);
                 if(nowNAlphabet<=nAlphabet){ // <
                     if(nChar[s[p2]-'a']==0)nowNAlphabet++;
                     nChar[s[p2]-'a']++;
@@ -43,15 +40,9 @@ public:
                     len--;
                     p1++;                    
                 }
-                // printf("now = %d  이번 턴 최대 알파벳 수 = %d, p1 = %d p2 = %d len = %d\n", nowNAlphabet, nAlphabet,p1,p2,len);
-                //printf("nChar = %d\n",nChar[s[p2]-'a']);
-                //printf("%d %d %d\n\n",nowNAlphabet, nAlphabet, sAlphabet);
-                if(nowNAlphabet == nAlphabet && nowNAlphabet == sAlphabet){
-                    //printf("len = %d, ans = %d\n",len,ans);
+                if(nowNAlphabet == nAlphabet && nowNAlphabet == sAlphabet)
                     ans = max(len,ans);
-                }
             }
-            //printf("==============\n");
         }
         return ans;
     }
