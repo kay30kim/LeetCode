208cdeef992b444cbe945c807b2d22c179d1defd
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2022-08-06 08:46:32 +0900

    Time: 408 ms (48.37%), Space: 24.6 MB (39.01%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,18 +1,18 @@
 class Solution {
-    unordered_map<int,int> hash;    
+private:
+    unordered_map<int,int> hash;
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         for(int i=0; i<nums1.size(); i++){
             for(int j=0; j<nums2.size(); j++){
-                hash[-nums1[i]-nums2[j]]++;
+                hash[nums1[i]+nums2[j]]++;
             }
         }
         int ans=0;
         for(int i=0; i<nums3.size(); i++){
-            for(int j=0; j<nums4.size(); j++){
-                if(hash.find(nums3[i]+nums4[j])!=hash.end()){
-                    ans+=hash[nums3[i]+nums4[j]];
-                }
+            for(int j=0;j<nums4.size(); j++){
+                if(hash[-nums3[i]-nums4[j]])
+                    ans+=hash[-nums3[i]-nums4[j]];
             }
         }
         return ans;
