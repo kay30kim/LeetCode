38f2db6d8f2526eb46e684d02bdea46a061a68e3
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2023-02-20 09:04:57 +0900

    Time: 6 ms (29.77%), Space: 12 MB (78.58%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -10,44 +10,54 @@
  * };
  */
 class Solution {
-public:
-    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-         vector<vector<int>> ans;
-         int cnt=0;
-        if(root==NULL)
-        return ans;
-        vector<int> helper;
-        queue<TreeNode*> q;
-        q.push(root);
-        q.push(NULL);
-        while(!q.empty())
-        {
-            TreeNode *temp=q.front();
-            if(temp==NULL)
-            {
-                q.pop();
-                if(!q.empty())
-                q.push(NULL);
-                if(cnt%2==0)
-                ans.push_back(helper);
-                else
-                {
-                    reverse(helper.begin(),helper.end());
-                    ans.push_back(helper);
-                }
-                cnt++;
-                helper.clear();
-            }
-            else
-            {
-                q.pop();
-                helper.push_back(temp->val);
-                if(temp->left)
-                q.push(temp->left);
-                if(temp->right)
-                q.push(temp->right);
-            }
+public : 
+vector<vector<int>> zigzagLevelOrder(TreeNode* root){
+    typedef struct{
+        TreeNode* treeNode;
+        int distance;
+    }Node;
+    vector<vector<int>> ans;
+
+    if(!root) return ans;
+    
+    queue<Node> q;
+    q.push({root,0});
+    int nowDistance = 0;
+    while(!q.empty()){
+        nowDistance=q.front().distance;
+        vector<int> tmpVector;
+        while(!q.empty() && q.front().distance==nowDistance){            
+            Node tmp = q.front();
+            printf("%d %d %d\n",tmp.treeNode->val,tmp.distance, nowDistance);
+            q.pop();
+            
+            if(tmp.treeNode && tmp.treeNode->left != NULL) q.push({tmp.treeNode->left,nowDistance+1});
+            if(tmp.treeNode && tmp.treeNode->right != NULL) q.push({tmp.treeNode->right,nowDistance+1});
+            tmpVector.push_back(tmp.treeNode->val);
+            
         }
-        return ans;
+        if(nowDistance%2 == 1){
+            for(int i=0; i<tmpVector.size(); i++){
+                printf("%d ",tmpVector[i]);
+            }
+            printf("\n");
+            reverse(tmpVector.begin(),tmpVector.end());
+            for(int i=0; i<tmpVector.size(); i++){
+                printf("%d ",tmpVector[i]);
+            }
+            printf("\n");
+            ans.push_back(tmpVector);
+        }
+        else{
+            
+            ans.push_back(tmpVector);
+            //printf("%d\n", )
+            //tmpVector = tmpVector.reserve(tmpVector.size());
+            
+        }
     }
+
+    return ans;
+
+}
 };
