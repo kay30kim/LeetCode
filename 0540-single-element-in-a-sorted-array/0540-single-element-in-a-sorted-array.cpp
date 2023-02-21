9a787010ff6a56a4abe514a51337785aa4b03844
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2023-02-21 23:45:05 +0900

    Time: 28 ms (67.14%), Space: 22.4 MB (62.74%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i += 2) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+        }
+        return nums.back();
+    }
+};
