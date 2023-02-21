feb8b31c3b818603b13293bfaaa4e9d11ce84fe5
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2023-02-22 08:31:49 +0900

    Time: 23 ms (91.45%), Space: 22.5 MB (16.49%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,11 +1,29 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        for (int i = 0; i < nums.size() - 1; i += 2) {
-            if (nums[i] != nums[i + 1]) {
-                return nums[i];
+        if(nums.size()==1) return nums[0];
+        int lo = 0, hi = nums.size()-1, mid;
+        while(lo<=hi){
+            mid = (lo+hi)/2;
+            //printf("lo = %d, hi = %d, mid = %d nums[mid] = %d %d\n",lo, hi,mid,nums[mid],nums[mid+1]);
+            if(mid-1>=0 && nums[mid]==nums[mid-1]){
+                if((mid-1)%2==1){
+                    hi=mid-2;
+                }else{
+                    lo=mid+1;
+                }
+            }else if(mid+1<nums.size() && nums[mid]==nums[mid+1]){
+                if((mid)%2==1){
+                    hi=mid-1;
+                }else{
+                    lo=mid+2;
+                }
+            }else{
+                //printf("come");
+                return nums[mid];
             }
+            printf("%d %d %d\n",lo,mid,hi);
         }
-        return nums.back();
+        return 0;
     }
 };
