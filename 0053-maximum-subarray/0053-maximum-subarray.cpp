2960465cc3b97a50cc3cc259e9f3090e33229a10
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2023-02-23 18:49:43 +0900

    Time: 483 ms (5.03%), Space: 67.8 MB (18.02%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0, ans=nums[0];
+        for(auto num:nums){
+            sum+=num;
+            if(sum<num) sum=num;
+            if(sum>ans) ans=sum;
+            printf("%d %d\n",sum,ans);
+        }
+        return ans;
+    }
+};
