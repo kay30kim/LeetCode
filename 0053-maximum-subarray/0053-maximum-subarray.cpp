31d4c33573ca2b390b7c9a198a9a6b05bf5f91cb
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2023-02-23 20:24:56 +0900

    Time: 175 ms (10.34%), Space: 67.7 MB (63.21%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int sum=0, ans=nums[0];
-        for(auto num:nums){
-            sum+=num;
-            if(sum<num) sum=num;
-            if(sum>ans) ans=sum;
-            //printf("%d %d\n",sum,ans);
+    int divideConquer(int left, int right, vector<int>& nums){
+        if(left>=right) return nums[left];
+        
+        int mid = (left+right)/2;
+        int curr = 0;
+        int bestLeftSum = 0;
+        int bestRightSum = 0;
+        //printf("%d %d %d\n",left,mid,right);
+        for(int i=mid-1 ; i >=left; i--){
+            curr += nums[i];
+            bestLeftSum = max(bestLeftSum,curr);
+            //printf("%d %d\n",i,nums[i]);
         }
-        return ans;
+        
+        curr = 0;
+        for(int i=mid+1; i<=right; i++){
+            //printf("%d %d\n",i,nums[i]);
+            curr +=nums[i];
+            bestRightSum = max(bestRightSum,curr);
+        }
+        
+        int bestCombinSum = nums[mid] + bestLeftSum + bestRightSum;
+        
+        int leftHalf = divideConquer(left,mid-1,nums);
+        int rightHalf = divideConquer(mid+1,right,nums);
+        //printf("%d %d %d\n",bestCombinSum,leftHalf,rightHalf);
+        return max(bestCombinSum, max(leftHalf, rightHalf));
+    }
+    int maxSubArray(vector<int>& nums) {
+        return divideConquer(0,nums.size()-1,nums);
     }
 };
