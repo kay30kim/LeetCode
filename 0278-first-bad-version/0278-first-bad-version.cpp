10e89c9aa21a6f3ee276fe051332f54100f23b86
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2023-03-01 22:55:51 +0900

    Time: 0 ms (100.00%), Space: 6 MB (57.57%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -0,0 +1,20 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+private:
+    int ans=0;
+public:
+    void binarySearch(int left, int right){
+        int mid = left - (left-right)/2;
+        if(isBadVersion(mid)) {
+            ans=mid; 
+            if(left!=right) binarySearch(left,mid);
+        }
+        else if(left!=right) binarySearch(mid+1,right);
+    }
+    int firstBadVersion(int n) {
+        binarySearch(1,n);
+        return ans;
+    }
+};
