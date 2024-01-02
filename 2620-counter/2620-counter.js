321056d41b478242ed95b4248d976b4ce0b41f28
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2024-01-02 09:37:07 +0900

    Time: 56 ms (24.51%), Space: 41.3 MB (93.94%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -6,6 +6,10 @@ var createCounter = function(n) {
     let num = n;
     return () => n++;
     // return () => {n++} 이렇게 하면 null이 나온다.
+    /* 이거는 가능
+    return function() {
+        return n++;
+    }*/
 }
 /** 
  * const counter = createCounter(10)
