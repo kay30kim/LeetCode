661f65d87aab45d0780ba654ebcaffef2c163f2e
Kay Kim <59407632+kay30kim@users.noreply.github.com>
2024-01-04 12:07:25 +0900

    Time: 56 ms (31.14%), Space: 42 MB (42.74%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,14 +4,19 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    // 1) don't Declare
+    // 1) In-Memory
     // for (let i = 0; i <arr.length; i++)
     //     arr[i] = fn(arr[i], i);
     // return arr;
     // 2) Declare arrary
-    const newArr = new Array(arr.length);
-    for (const i in arr) {
-        newArr[i] = fn(arr[i], Number(i));
+    // const newArr = new Array(arr.length);
+    // for (const i in arr) {
+    //     newArr[i] = fn(arr[i], Number(i)); // 그냥 i는 char
+    // }
+    // 3) Push
+    const newArr = [];
+    for (let i = 0; i <arr.length; i++) {
+        newArr.push(fn(arr[i], i));
     }
     return newArr;
 };
